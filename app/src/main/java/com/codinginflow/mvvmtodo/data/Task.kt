939d5153b786e17eb797059e53f9687ad70d2162
Alex Maina <maina.alex@eclectics.io>
2022-02-24 23:19:49 +0300

    Added Fragments and navigation graph

---
+++ app/src/main/java/com/codinginflow/mvvmtodo/data/Task.kt
@@ -0,0 +1,22 @@
+package com.codinginflow.mvvmtodo.data
+
+import android.os.Parcelable
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import kotlinx.android.parcel.Parcelize
+import java.text.DateFormat
+import java.util.*
+
+@Entity(tableName = "task_table")
+@Parcelize
+data class Task(
+    @PrimaryKey(autoGenerate = true)
+    val id:Int=0,
+    val name:String,
+    val important: Boolean=false,
+    val created: Long= System.currentTimeMillis(),
+    val completed:Boolean=false
+    ): Parcelable {
+    val createdDateFormatted:String
+    get()= DateFormat.getDateInstance().format(created)
+}
